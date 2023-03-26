e3b776d823058766b000ef77c1b23d47724b5144
mikey <mforest@protonmail.com>
2023-03-26 16:16:07 -0400

    added draft components

--- .output/server/chunks/app/_nuxt/theHero5-styles.9e1e8e7a.mjs
+++
@@ -1,6 +0,0 @@
-const theHero5_vue_vue_type_style_index_0_scoped_c1dfb946_lang = '@keyframes night-nebula-sky-c1dfb946{0%{background-position:0 0}to{background-position:100% 100%}}.animate-night-nebula-sky[data-v-c1dfb946]{animation:night-nebula-sky-c1dfb946 8s ease infinite;background:linear-gradient(135deg,#1c1e3f 25%,#2f2a5a 50%,#7d778b 75%);background-size:200% 200%}.overlay-pattern[data-v-c1dfb946]:before{background-image:radial-gradient(circle,hsla(0,0%,8%,.2) 25%,transparent 0,transparent 50%,hsla(0,0%,8%,.2) 0,hsla(0,0%,8%,.2) 75%,transparent 0,transparent);background-size:10px 10px;bottom:0;content:"";display:block;left:0;position:absolute;right:0;top:0}';
-
-const theHero5Styles_9e1e8e7a = [theHero5_vue_vue_type_style_index_0_scoped_c1dfb946_lang];
-
-export { theHero5Styles_9e1e8e7a as default };
-//# sourceMappingURL=theHero5-styles.9e1e8e7a.mjs.map
