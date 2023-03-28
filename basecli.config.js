d769443f5cad936b7727da9ae1cc08a400e269fd
mikey <mforest@protonmail.com>
2023-03-28 14:40:01 -0400

    Accepting some configs for default theme

--- basecli.config.js
+++ basecli.config.js
@@ -1,1 +1,9 @@
-module.exports = {"message":"Hello, World!"};
+
+      module.exports = {
+        primaryColor: 'bg-blue-500 text-white',
+        secondaryColor: 'bg-gray-500 text-white',
+        accentColor: 'bg-green-500 text-white',
+        backgroundColor: 'bg-gray-100',
+        buttonRadius: 'rounded-md'
+      };
+    
--- basecli.js
+++ basecli.js
@@ -9,10 +9,22 @@ program
   .command('init')
   .description('Initialize the config file')
   .option('-o, --output <outputFile>', 'Specify output file', 'basecli.config.js')
-  .option('-m, --message <message>', 'Specify a message', 'Hello, World!')
+  .option('-p, --primary <color>', 'Specify primary color', 'bg-blue-500 text-white')
+  .option('-s, --secondary <color>', 'Specify secondary color', 'bg-gray-500 text-white')
+  .option('-a, --accent <color>', 'Specify accent color', 'bg-green-500 text-white')
+  .option('-b, --background <color>', 'Specify background color', 'bg-gray-100')
+  .option('-r, --radius <radius>', 'Specify button radius', 'rounded-md')
   .action(async (cmd) => {
     const filePath = path.join(process.cwd(), cmd.output);
-    const data = `module.exports = ${JSON.stringify({ message: cmd.message })};\n`;
+    const data = `
+      module.exports = {
+        primaryColor: '${cmd.primary}',
+        secondaryColor: '${cmd.secondary}',
+        accentColor: '${cmd.accent}',
+        backgroundColor: '${cmd.background}',
+        buttonRadius: '${cmd.radius}'
+      };
+    `;
 
     try {
       await fs.promises.writeFile(filePath, data);
