c82ac96143594e506f4e0d7d7e835fad3b04b6ce
mikey <mforest@protonmail.com>
2023-03-28 14:51:24 -0400

    Added some additional configs

--- basecli.config.js
+++ basecli.config.js
@@ -3,6 +3,8 @@
         primaryColor: 'bg-blue-500 text-white',
         secondaryColor: 'bg-gray-500 text-white',
         accentColor: 'bg-green-500 text-white',
+        helperText: 'text-gray-500',
+        linkText: 'text-blue-500',
         backgroundColor: 'bg-gray-100',
         buttonRadius: 'rounded-md'
       };
--- basecli.js
+++ basecli.js
@@ -9,18 +9,25 @@ program
   .command('init')
   .description('Initialize the config file')
   .option('-o, --output <outputFile>', 'Specify output file', 'basecli.config.js')
-  .option('-p, --primary <color>', 'Specify primary color', 'bg-blue-500 text-white')
-  .option('-s, --secondary <color>', 'Specify secondary color', 'bg-gray-500 text-white')
-  .option('-a, --accent <color>', 'Specify accent color', 'bg-green-500 text-white')
+  .option('-p, --primary-bg <color>', 'Specify primary background color', 'bg-blue-500')
+  .option('-p-t, --primary-text <color>', 'Specify primary text color', 'text-white')
+  .option('-s, --secondary-bg <color>', 'Specify secondary background color', 'bg-gray-500')
+  .option('-s-t, --secondary-text <color>', 'Specify secondary text color', 'text-white')
+  .option('-h, --helper <color>', 'Specify helper text color', 'text-gray-500')
+  .option('-l, --link <color>', 'Specify link text color', 'text-blue-500')
+  .option('-a, --accent-bg <color>', 'Specify accent background color', 'bg-green-500')
+  .option('-a-t, --accent-text <color>', 'Specify accent text color', 'text-white')
   .option('-b, --background <color>', 'Specify background color', 'bg-gray-100')
   .option('-r, --radius <radius>', 'Specify button radius', 'rounded-md')
   .action(async (cmd) => {
     const filePath = path.join(process.cwd(), cmd.output);
     const data = `
       module.exports = {
-        primaryColor: '${cmd.primary}',
-        secondaryColor: '${cmd.secondary}',
-        accentColor: '${cmd.accent}',
+        primaryColor: '${cmd.primaryBg} ${cmd.primaryText}',
+        secondaryColor: '${cmd.secondaryBg} ${cmd.secondaryText}',
+        accentColor: '${cmd.accentBg} ${cmd.accentText}',
+        helperText: '${cmd.helper}',
+        linkText: '${cmd.link}',
         backgroundColor: '${cmd.background}',
         buttonRadius: '${cmd.radius}'
       };
