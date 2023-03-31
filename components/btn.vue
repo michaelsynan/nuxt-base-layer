1635d281a16e797cd1c38c212623e2fe1313e150
mikey <mforest@protonmail.com>
2023-03-31 08:54:49 -0400

    Added button variant props

--- components/btn.vue
+++ components/btn.vue
@@ -6,6 +6,27 @@
   </button>
 </template>
 
+<style>
+.hard-shadow {
+  box-shadow: 0 4px 0 #000;
+}
+.hover-flat:hover {
+  background-color: #e2e8f0;
+}
+.hover-raised:hover {
+  background-color: #4299e1;
+}
+.hover-ghost:hover {
+  background-color: #e2e8f0;
+}
+.hover-hard-shadow:hover {
+  background-color: #9f7aea;
+}
+.hover-default:hover {
+  background-color: #edf2f7;
+}
+</style>
+
 <script setup>
 import { defineProps } from 'vue';
 
@@ -16,20 +37,22 @@ const props = defineProps({
   }
 });
 
-const buttonClass = `rounded-lg px-4 py-2 ${getButtonStyle(props.type)}`;
+const buttonClass = `rounded-lg px-4 py-2 transition-all duration-200 ease-in-out ${getButtonStyle(props.type)}`;
 
 function getButtonStyle(type) {
   switch (type) {
     case 'flat':
-      return 'bg-gray-100 text-gray-700';
+      return 'bg-gray-100 text-gray-700 hover-flat';
     case 'raised':
-      return 'bg-blue-500 text-white shadow-md';
+      return 'bg-blue-500 text-white shadow-md hover-raised';
     case 'ghost':
-      return 'bg-transparent text-gray-700';
+      return 'bg-transparent text-gray-700 border-gray-700 border hover-ghost';
     case 'hard-shadow':
-      return 'bg-purple-500 text-white shadow-xl';
+      return 'bg-purple-500 text-white hard-shadow hover-hard-shadow';
     default:
-      return 'bg-gray-200 text-gray-700';
+      return 'bg-gray-200 text-gray-700 hover-default';
   }
 }
 </script>
+
+
---
+++ pages/components.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Components</h1>
+    <div>
+      <h2>Buttons</h2>
+      <div class="flex space-x-4 p-4">
+        <btn />
+        <btn type="flat" />
+        <btn type="raised" />
+        <btn type="ghost" />
+        <btn type="hard-shadow" />
+      </div>
+    </div>
+  </div>
+</template>
